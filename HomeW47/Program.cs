14568e143683f31ca31fbfa42ed85a44104b5cb9
YZ318 <YakovZhuravlev88@gmail.com>
2023-05-19 08:21:35 +0300

    Задача 47

---
+++ HomeW47/Program.cs
@@ -0,0 +1,20 @@
+﻿double[,] array = new double[3, 4];
+Random rnd = new Random();
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        array[i, j] = rnd.NextDouble() * 20 - 10;
+    }
+}
+
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
