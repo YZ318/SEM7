bb252e74a6b8fe7ef6460c0e8eabf656369de387
YZ318 <YakovZhuravlev88@gmail.com>
2023-05-19 08:35:10 +0300

    Задача 50

---
+++ HomeW50/Program.cs
@@ -0,0 +1,19 @@
+﻿int[,] array = new int[3, 4] {
+    {1, 4, 7, 2},
+    {5, 9, 2, 3},
+    {8, 4, 2, 4}
+};
+
+Console.Write("Введите номер строки: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Введите номер столбца: ");
+int col = int.Parse(Console.ReadLine());
+
+if (row < 0 || row >= array.GetLength(0) || col < 0 || col >= array.GetLength(1))
+{
+    Console.WriteLine("Такого элемента в массиве нет");
+}
+else
+{
+    Console.WriteLine("Значение элемента: " + array[row, col]);
+}
